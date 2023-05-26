a1fecd23ecdc8ba0cbcf6d0f1247ca351cea407d
Ranidu Harshana <harshanaranidum@gmail.com>
2023-05-26 17:27:10 +0530

    initial commit

---
+++ sri-kula-madura-server/src/test/java/com/SriKulaMadura/server/SriKulaMaduraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SriKulaMadura.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriKulaMaduraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
