3ba199ce0cd8dd1180d390b4d614e7bca0389727
Ranidu Harshana <harshanaranidum@gmail.com>
2023-06-06 17:39:21 +0530

    initial setup

--- sri-kula-medura-control/src/layouts/MainLayout.jsx
+++ sri-kula-medura-control/src/layouts/MainLayout.jsx
@@ -1,6 +1,16 @@
+import Header from "../components/Header/Header";
+import SideNav from "../components/SideNav/SideNav";
+import Footer from "../components/Footer/Footer";
+import {Outlet} from "react-router-dom";
+
 const MainLayout = (props) => {
 	return (
-		<div>MainLayout</div>
+		<>
+			<Header />
+			<SideNav />
+			<Outlet />
+			<Footer />
+		</>
 	);
 }
 
--- sri-kula-medura-control/src/routers/AppRouter.jsx
+++ sri-kula-medura-control/src/routers/AppRouter.jsx
@@ -1,39 +1,35 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import MainLayout from "../Layout/MainLayout";
-import Home from "../Pages/Header/Header";
-import {Login} from "@mui/icons-material";
-import UserProtected from "./UserProtected";
-import User from "../Pages/User/User";
-import Category from "../Pages/Category/Category";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
-	// {
-	// 	path:"/",
-	// 	element: <MainLayout />,
-	// 	children: [
-	// 		{
-	// 			index: true,
-	// 			element: <Home />
-	// 		},
-	// 		{
-	// 			path: 'category',
-	// 			element: <Category />
-	// 		},
-	// 		{
-	// 			path: "login",
-	// 			element: <Login />
-	// 		},
-	// 		{
-	// 			element: <UserProtected />,
-	// 			children: [
-	// 				{
-	// 					path: "user",
-	// 					element: <User />
-	// 				}
-	// 			]
-	// 		}
-	// 	]
-	// }
+	{
+		path:"/",
+		element: <MainLayout />,
+		children: [
+			{
+				index: true,
+				element: <Home />
+			},
+			// {
+			// 	path: 'category',
+			// 	element: <Category />
+			// },
+			// {
+			// 	path: "login",
+			// 	element: <Login />
+			// },
+			// {
+			// 	element: <UserProtected />,
+			// 	children: [
+			// 		{
+			// 			path: "user",
+			// 			element: <User />
+			// 		}
+			// 	]
+			// }
+		]
+	}
 ])
 const AppRouter = (props) => {
 	return (
