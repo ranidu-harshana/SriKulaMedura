72be871adb7752e34768ee25089e0ef703aa4622
Ranidu Harshana <harshanaranidum@gmail.com>
2023-06-06 22:58:30 +0530

    setup

--- sri-kula-medura-control/src/layouts/MainLayout.jsx
+++ sri-kula-medura-control/src/layouts/MainLayout.jsx
@@ -6,10 +6,16 @@ import {Outlet} from "react-router-dom";
 const MainLayout = (props) => {
 	return (
 		<>
-			<Header />
-			<SideNav />
-			<Outlet />
-			<Footer />
+			<div className="flex flex-row">
+				<Header />
+			</div>
+			<div className="flex flex-row">
+				<div className="basis-1/4"><SideNav /></div>
+				<div className="basis-3/4"><Outlet /></div>
+			</div>
+			<div className="flex flex-row">
+				<Footer />
+			</div>
 		</>
 	);
 }
