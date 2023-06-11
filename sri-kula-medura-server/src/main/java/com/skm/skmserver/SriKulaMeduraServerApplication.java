066c5f84db701ff5a0f5ebb1519dada928a85796
Ranidu Harshana <harshanaranidum@gmail.com>
2023-06-11 13:54:58 +0530

    updated server configurations

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/SriKulaMeduraServerApplication.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/SriKulaMeduraServerApplication.java
@@ -1,13 +1,13 @@
-package com.SriKulaMadura.server;
+package com.skm.skmserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SriKulaMaduraServerApplication {
+public class SriKulaMeduraServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SriKulaMaduraServerApplication.class, args);
+		SpringApplication.run(SriKulaMeduraServerApplication.class, args);
 	}
 
 }
--- sri-kula-medura-server/src/test/java/com/skm/skmserver/SriKulaMeduraServerApplicationTests.java
+++ sri-kula-medura-server/src/test/java/com/skm/skmserver/SriKulaMeduraServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.SriKulaMadura.server;
+package com.skm.skmserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SriKulaMaduraServerApplicationTests {
+class SriKulaMeduraServerApplicationTests {
 
 	@Test
 	void contextLoads() {
