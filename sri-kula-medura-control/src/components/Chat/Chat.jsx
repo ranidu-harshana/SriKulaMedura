cf51140af1052ebc0937178d3f2c50f770c89d61
Ranidu Harshana <harshanaranidum@gmail.com>
2023-06-15 20:19:52 +0530

    basic structure of chat app

--- sri-kula-medura-control/src/components/Chat/Chat.jsx
+++ sri-kula-medura-control/src/components/Chat/Chat.jsx
@@ -1,19 +1,26 @@
 import useravatar from '../../assets/chat/patient-thumb-02.jpg'
 import {Link} from "react-router-dom";
+import './Chat.css'
+import Button from "@mui/material/Button";
+import SendIcon from '@mui/icons-material/Send';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+
 
 const Chat = (props) => {
 	return (
 		<div>
-			<div className="d-flex p-2" style={{backgroundColor: "white", borderBottom: "1px solid #eeee"}}>
-				<div style={{width: "45px"}} className={`my-1 ms-3`}>
-					<img src={useravatar} width="100%" alt="" className="rounded-circle"/>
-				</div>
-				<div className="mx-2 my-1">
-					<Link to={``} className={`text-decoration-none`}>
-						<span className="font-bold text-uppercase chat-user-name">Sachini Deepashika</span>
-						<i className="typing-text ms-1">Typing...</i>
-					</Link>
-					<span className="last-seen">Last seen today at 7:50 AM</span>
+			<div className="row">
+				<div className="d-flex p-2 chat-header">
+					<div style={{width: "45px"}} className={`my-1 ms-3`}>
+						<img src={useravatar} width="100%" alt="" className="rounded-circle"/>
+					</div>
+					<div className="mx-2 my-1">
+						<Link to={``} className={`text-decoration-none`}>
+							<span className="font-bold text-uppercase chat-user-name">Malsha Hasanjali</span>
+							<i className="typing-text ms-1">Typing...</i>
+						</Link>
+						<span className="last-seen">Last seen today at 7:50 AM</span>
+					</div>
 				</div>
 			</div>
 
@@ -59,34 +66,16 @@ const Chat = (props) => {
 											<span className="chat-time">8:40 am</span>
 										</div>
 									</div>
-								</div>
-							</div>
-
-							<div className="chat chat-left">
-								<div className="chat-body">
-									<div className="chat-bubble">
-										<div className="chat-content">
-											<p>I'm just looking around.</p>
-											<p>Will you tell me something about yourself? </p>
-											<span className="chat-time">8:35 am</span>
-										</div>
-									</div>
 									<div className="chat-bubble">
 										<div className="chat-content">
 											<p>Are you there? That time!</p>
 											<span className="chat-time">8:40 am</span>
 										</div>
 									</div>
-								</div>
-							</div>
-
-							<div className="chat chat-left">
-								<div className="chat-body">
 									<div className="chat-bubble">
 										<div className="chat-content">
-											<p>I'm just looking around.</p>
-											<p>Will you tell me something about yourself? </p>
-											<span className="chat-time">8:35 am</span>
+											<p>Are you there? That time!</p>
+											<span className="chat-time">8:40 am</span>
 										</div>
 									</div>
 									<div className="chat-bubble">
@@ -102,20 +91,15 @@ const Chat = (props) => {
 				</div>
 			</div>
 
-			<div className="chat-footer">
-				<div className="message-bar">
-					<div className="message-inner">
-						<a className="link attach-icon" href="#" data-toggle="modal" data-target="#drag_files"><img
-							src="assets/img/attachment.png" alt=""/></a>
-						<div className="message-area">
-							<div className="input-group">
-								<textarea className="form-control" placeholder="Type message..."></textarea>
-								<span className="input-group-append">
-													<button className="btn btn-primary" type="button"><i
-														className="fa fa-send"></i></button>
-												</span>
-							</div>
-						</div>
+			<div className="row">
+				<div className="chat-footer">
+					<div className="input-group">
+						<TextareaAutosize className={"form-control"}/>
+						<span className="input-group-append">
+								<Button variant="contained" sx={{height: "100%", border: "none"}}>
+									<SendIcon/>
+								</Button>
+							</span>
 					</div>
 				</div>
 			</div>
