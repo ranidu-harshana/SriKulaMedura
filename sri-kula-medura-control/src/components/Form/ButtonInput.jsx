d195d43008ec6944911ea64e7c4defa6280d534a
Danilka <73026664+DanilkaShalinda@users.noreply.github.com>
2023-06-19 16:28:07 +0530

    Input Button Component created

---
+++ sri-kula-medura-control/src/components/Form/ButtonInput.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from "@mui/material/Button";
+
+const ButtonInput = () => {
+    const inputTypes = [
+        {
+            name: 'Button',
+            label: 'Button',
+            type: 'Button',
+            id: 'Button',
+            variant : '',
+            startIcon :'',
+            endIcon :'',
+            onClick : '',
+            href :''
+
+        }
+    ];
+
+    return (
+        <>
+            {inputTypes.map((inputType, index) => (
+                <Button {...inputType} key={index} />
+            ))}
+        </>
+    );
+};
+
+export default ButtonInput;
+Footer
