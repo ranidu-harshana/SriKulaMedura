77853e9e1fa39d321e505857567c57192cf22097
SachiDeepashika <114232848+SachiDeepashika@users.noreply.github.com>
2023-06-23 21:00:11 +0530

    site navbar created

---
+++ sri-kula-medura-site/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import "./Navbar.css";
+
+export const UserLogin = () => {
+    return (
+        <div className="user-login">
+            <div className="div">
+                <div className="overlap">
+                    <div className="overlap-group">
+                        <img className="account-circle" alt="Account circle" src="account-circle.png"/>
+                        <h1 className="text-wrapper">LOGIN</h1>
+                    </div>
+                    <div className="div-wrapper">
+                        <div className="text-wrapper-2">Username</div>
+                    </div>
+                    <div className="overlap-2">
+                        <div className="text-wrapper-3">Password</div>
+                    </div>
+                    <div className="overlap-3">
+                        <div className="rectangle"/>
+                        <div className="text-wrapper-4">LOGIN</div>
+                    </div>
+                    <div className="text-wrapper-5">Forgot Password ?</div>
+                    <div className="rectangle-2"/>
+                    <img className="icon-rounded" alt="Icon rounded" src="icon-rounded-facebook.png"/>
+                    <img className="img" alt="Icon rounded" src="icon-rounded-twitterbird.png"/>
+                    <img className="icon-rounded-2" alt="Icon rounded" src="icon-rounded-linkedin.png"/>
+                </div>
+                <div className="navigation-bar">
+                    <div className="overlap-group-2">
+                        <div className="overlap-4">
+                            <div className="divider"/>
+                            <p className="home-about-us-FAQ">
+                                Home&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; About
+                                Us&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; FAQ
+                            </p>
+                        </div>
+                        <img className="facebook-ficon" alt="Facebook ficon" src="facebook-ficon.png"/>
+                        <img className="instagram-ficon" alt="Instagram ficon" src="instagram-ficon.png"/>
+                        <img className="sri-kula-medura-logo" alt="Sri kula medura logo"
+                             src="logo.png"/>
+                    </div>
+                </div>
+                <div className="section">
+                    <div className="copyright-sri-wrapper">
+                        <p className="copyright-sri"> Copyright @ 2023 Sri Kula Medura | All Rights Reserved.</p>
+                    </div>
+                </div>
+                <div className="overlap-wrapper">
+                    <div className="overlap-5">
+                        <div className="text-wrapper-6">Quick Links</div>
+                        <div className="text-wrapper-7">Contact Us</div>
+                        <div className="text-wrapper-8">Our Location</div>
+                        <div className="flex-container">
+                            <div className="text">
+                <span className="span">
+                  About Us
+                  <br/>
+                </span>
+                            </div>
+                            <div className="span-wrapper">
+                <span className="text-wrapper-9">
+                  Contact Us
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-2">
+                <span className="text-wrapper-10">
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-3">
+                <span className="text-wrapper-11">
+                  Privacy Policy
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-4">
+                <span className="text-wrapper-12">
+                  Refund Policy
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-5">
+                                <span className="text-wrapper-13">Terms and Conditions</span>
+                            </div>
+                        </div>
+                        <div className="flex-container-2">
+                            <div className="text-6">
+                <span className="text-wrapper-14">
+                  No.xxx,
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-7">
+                <span className="text-wrapper-15">
+                  Street 01
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-8">
+                <span className="text-wrapper-16">
+                  Street 02(optional)
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-9">
+                <span className="text-wrapper-17">
+                  Town
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-10">
+                <span className="text-wrapper-18">
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-11">
+                <span className="text-wrapper-19">
+                  Email
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-12">
+                <span className="text-wrapper-20">
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-13">
+                <span className="text-wrapper-21">
+                  07xxxxxxxxxx
+                  <br/>
+                </span>
+                            </div>
+                            <div className="text-14">
+                                <span className="text-wrapper-22">07xxxxxxxxxx</span>
+                            </div>
+                        </div>
+                        <img className="rectangle-3" alt="Rectangle" src="rectangle.png"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserLogin;
