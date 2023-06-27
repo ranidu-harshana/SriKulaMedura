d544034691cc8a2a3bc24675696e4b759e55f8e7
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-06-28 01:28:13 +0530

    branch entity updated

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/entity/branch.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/entity/branch.java
@@ -1,4 +1,31 @@
 package com.skm.skmserver.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class branch {
+    @Id
+    private String id;
+
+    private String name;
+
+    private boolean status;
+
+    private String prefix;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date created_at;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updated_at;
 }
