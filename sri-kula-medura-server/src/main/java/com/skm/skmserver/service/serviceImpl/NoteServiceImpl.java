614d6069c1be5b2912f1d60379546fd2fb7b6fd0
Ranidu Harshana <harshanaranidum@gmail.com>
2023-06-28 14:49:38 +0530

    note layers

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/NoteServiceImpl.java
@@ -0,0 +1,8 @@
+package com.skm.skmserver.service.serviceImpl;
+
+import com.skm.skmserver.service.NoteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteServiceImpl implements NoteService {
+}
