7a02d04f8bedf70b2632796c21055fc7848445ca
Ranidu Harshana <harshanaranidum@gmail.com>
2023-06-28 14:49:16 +0530

    changed the service annotation

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ItemServiceImpl.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ItemServiceImpl.java
@@ -1,6 +1,8 @@
 package com.skm.skmserver.service.serviceImpl;
 
 import com.skm.skmserver.service.ItemService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemServiceImpl implements ItemService {
 }
