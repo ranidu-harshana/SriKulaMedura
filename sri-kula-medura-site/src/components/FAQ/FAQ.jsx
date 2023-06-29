b37b65a8efd990b37de5184b0967493dc0a0a0b5
SachiDeepashika <114232848+SachiDeepashika@users.noreply.github.com>
2023-06-29 14:41:19 +0530

    faq component

---
+++ sri-kula-medura-site/src/components/FAQ/FAQ.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./FAQ.css";
+
+export const FAQ = () => {
+    return (
+        <div className="box">
+            <div className="element-wrapper">
+                <div className="element">
+                    <div className="div">
+                        <img className="payment" alt="Payment" src="payment.svg" />
+                        <h1 className="text-wrapper">
+                            simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the lorem lorem ?
+                        </h1>
+                        <img className="drop-down" alt="Drop down" src="drop-down.png" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default FAQ;
