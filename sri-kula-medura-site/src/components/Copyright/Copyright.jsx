0fa0781133e88dfaa083e6f0a82bb5cf5e27a995
SachiDeepashika <114232848+SachiDeepashika@users.noreply.github.com>
2023-06-29 14:45:44 +0530

    copyright bar component

---
+++ sri-kula-medura-site/src/components/Copyright/Copyright.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Copyright.css";
+
+export const Copyright = () => {
+    return (
+        <div className="box">
+            <div className="section-wrapper">
+                <div className="section">
+                    <div className="overlap-group">
+                        <h1 className="copyright-sri"> Copyright @ 2023 Sri Kula Medura | All Rights Reserved.</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Copyright;
