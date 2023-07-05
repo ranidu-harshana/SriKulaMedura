1b2a0f704348d3b23417451d2827750a7d75ad47
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-05 20:06:17 +0530

    controllers added.

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/ItemController.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/ItemController.java
@@ -1,20 +1,54 @@
 package com.skm.skmserver.controller;
 
+import com.skm.skmserver.dto.BranchDTO;
+import com.skm.skmserver.dto.ItemDTO;
 import com.skm.skmserver.entity.Item;
 import com.skm.skmserver.repo.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.skm.skmserver.service.serviceImpl.ItemServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/v1/items")
 public class ItemController {
+    private final ItemServiceImpl itemService;
+    public ItemController(ItemServiceImpl itemService) {
+        this.itemService = itemService;
+    }
 
-    @GetMapping(value = "/")
-    public String hi () {
-        return "hi";
+    @GetMapping("/")
+    public List<ItemDTO> index(){
+        return itemService.allItems();
+    }
+
+    @PostMapping("/")
+    public ItemDTO store(@RequestBody ItemDTO itemDTO){
+        return itemService.saveItem(itemDTO);
+    }
+
+    @GetMapping("/create")
+    public String create(){
+        return "";
+    }
+
+    @GetMapping("/{id}")
+    public ItemDTO show(@PathVariable int id){
+        return itemService.getItem(id);
+    }
+
+    @PutMapping("/{id}")
+    public ItemDTO update(@RequestBody ItemDTO itemDTO, @PathVariable int id){
+        return itemService.updateItem(itemDTO, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id){
+        return "delete";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id){
+        return "edit";
     }
 }
