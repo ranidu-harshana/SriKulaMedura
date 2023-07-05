6b7a15a0f59ef7e4dd50c64827b7b1b6ac0455a2
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-05 19:16:07 +0530

    itemDTO created

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/ItemDTO.java
@@ -0,0 +1,41 @@
+package com.skm.skmserver.dto;
+
+import com.skm.skmserver.entity.DressSelection;
+import com.skm.skmserver.entity.ItemCategory;
+import com.skm.skmserver.entity.WishList;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Date;
+import java.util.List;
+
+public class ItemDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String item_code;
+    private String item_name;
+    private String item_type;
+    private String item_image_url;
+
+    @Column(nullable = false, updatable = false, columnDefinition = "DATE DEFAULT CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    private Date created_at;
+
+    @Column(nullable = false, columnDefinition = "DATE DEFAULT CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    private Date updated_at;
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT true")
+    private boolean rented_status; // to check if rented or not
+
+    @ManyToOne
+    private ItemCategory item_category;
+
+    @OneToMany(mappedBy = "item")
+    private List<DressSelection> dress_selections;
+
+    @OneToMany(mappedBy = "item")
+    private List<WishList> wishlist;
+}
