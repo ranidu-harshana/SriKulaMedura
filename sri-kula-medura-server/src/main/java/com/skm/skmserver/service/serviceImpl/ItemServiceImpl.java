54c657e8238d03e8871131cd16e288e3e5cc2002
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-05 19:53:37 +0530

    itemserviceimpl implemeted

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ItemServiceImpl.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ItemServiceImpl.java
@@ -1,8 +1,22 @@
 package com.skm.skmserver.service.serviceImpl;
 
+import com.skm.skmserver.dto.ItemDTO;
+import com.skm.skmserver.entity.Item;
+import com.skm.skmserver.repo.ItemRepository;
 import com.skm.skmserver.service.ItemService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemServiceImpl implements ItemService {
+    private final ItemRepository itemRepository;
+    private final ModelMapper modelMapper;
+    public ItemServiceImpl(ItemRepository iitemRepository, ModelMapper modelMapper){
+        this.itemRepository = iitemRepository;
+        this.modelMapper = modelMapper;
+    }
+    public ItemDTO saveItem(ItemDTO itemDTO){
+        itemRepository.save(modelMapper.map(itemDTO, Item.class));
+        return itemDTO;
+    }
 }
