dca46a7a64b056c7b1f5b4f4e9df77539f2cf477
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-08 03:15:01 +0530

    additional payment entity created

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/entity/AdditionalPayment.java
@@ -0,0 +1,39 @@
+package com.skm.skmserver.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AdditionalPayment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private double payment;
+
+    private String reason;
+
+    private boolean status;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date created_at;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    private Date updated_at;
+
+//    @ManyToOne
+//    private Reservation reservation;
+}
