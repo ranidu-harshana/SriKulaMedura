2b7cb120354b154b15796722bd4016592362f79f
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-07 23:59:53 +0530

    Update ItemCategoryController.java

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/ItemCategoryController.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/ItemCategoryController.java
@@ -8,36 +8,44 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "api/v1/itemcategory")
 public class ItemCategoryController {
-    private final ItemCategoryServiceImpl itemCategoryServiceimpl;
-     public ItemCategoryController(ItemCategoryServiceImpl itemCategoryServiceimpl) {
-         this.itemCategoryServiceimpl = itemCategoryServiceimpl;
-     }
-     @GetMapping("/")
-     public String index(){
-         return "Item Category";
-     }
-     @PostMapping("/")
-     public ItemCategoryDTO store(@RequestBody ItemCategoryDTO itemCategoryDTO){
-         return itemCategoryServiceimpl.saveItemCategory(itemCategoryDTO);
-     }
-     @GetMapping("/create")
-     public String create(){
-         return "Create Item Category";
-     }
-     @GetMapping("/{id}")
-     public String show(@PathVariable int id){
+    private final ItemCategoryServiceImpl itemCategoryService;
+
+    public ItemCategoryController(ItemCategoryServiceImpl itemCategoryService) {
+        this.itemCategoryService = itemCategoryService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Item Category";
+    }
+
+    @PostMapping("/")
+    public ItemCategoryDTO store(@RequestBody ItemCategoryDTO itemCategoryDTO) {
+        return itemCategoryService.saveItemCategory(itemCategoryDTO);
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "Create Item Category";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable int id) {
         return "Show Item Category :" + id;
-     }
-     @PutMapping("/{id}")
-     public String update(@RequestBody ItemDTO itemDTO,@PathVariable int id){
-         return "Update Item Category :" + id;
-     }
-     @DeleteMapping("/{id}")
-    public String delete(@PathVariable int id){
-         return "Delete Item Category :" + id;
-     }
-     @GetMapping("/{id}/edit")
-    public String edit(@PathVariable int id){
-         return "Edit Item Category :" + id;
-     }
+    }
+
+    @PutMapping("/{id}")
+    public String update(@RequestBody ItemDTO itemDTO, @PathVariable int id) {
+        return "Update Item Category :" + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id) {
+        return "Delete Item Category :" + id;
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id) {
+        return "Edit Item Category :" + id;
+    }
 }
