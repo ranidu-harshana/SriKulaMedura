900419df34aa965896d8ab662c6c929105415d47
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-07 13:36:30 +0530

    item category controller updated

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/ItemCategoryController.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/ItemCategoryController.java
@@ -1,9 +1,43 @@
 package com.skm.skmserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.skm.skmserver.dto.ItemCategoryDTO;
+import com.skm.skmserver.dto.ItemDTO;
+import com.skm.skmserver.service.serviceImpl.ItemCategoryServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/itemcategory")
 public class ItemCategoryController {
+    private final ItemCategoryServiceImpl itemCategoryServiceimpl;
+     public ItemCategoryController(ItemCategoryServiceImpl itemCategoryServiceimpl) {
+         this.itemCategoryServiceimpl = itemCategoryServiceimpl;
+     }
+     @GetMapping("/")
+     public String index(){
+         return "Item Category";
+     }
+     @PostMapping("/")
+     public ItemCategoryDTO store(@RequestBody ItemCategoryDTO itemCategoryDTO){
+         return itemCategoryServiceimpl.saveItemCategory(itemCategoryDTO);
+     }
+     @GetMapping("/create")
+     public String create(){
+         return "Create Item Category";
+     }
+     @GetMapping("/{id}")
+     public String show(@PathVariable int id){
+        return "Show Item Category :" + id;
+     }
+     @PutMapping("/{id}")
+     public String update(@RequestBody ItemDTO itemDTO,@PathVariable int id){
+         return "Update Item Category :" + id;
+     }
+     @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id){
+         return "Delete Item Category :" + id;
+     }
+     @GetMapping("/{id}/edit")
+    public String edit(@PathVariable int id){
+         return "Edit Item Category :" + id;
+     }
 }
