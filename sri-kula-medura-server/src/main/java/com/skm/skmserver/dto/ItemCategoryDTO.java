4fb8d28687cf068225e4cfe9e36fc47b2b705b72
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-07 13:36:51 +0530

    item category DTO updated

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/ItemCategoryDTO.java
@@ -0,0 +1,16 @@
+package com.skm.skmserver.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ItemCategoryDTO {
+    private int id;
+    private String category_name;
+    private Date created_at;
+    private Date updated_at;
+
+}
