6f94a47fb1980f9451f906c20ebfb4d4abaa56f2
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-08 16:10:32 +0530

    item category all the operations

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/ItemCategory/UpdateItemCategoryDTO.java
@@ -0,0 +1,12 @@
+package com.skm.skmserver.dto.ItemCategory;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UpdateItemCategoryDTO {
+    private String category_name;
+}
--- sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ItemCategoryServiceImpl.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ItemCategoryServiceImpl.java
@@ -1,13 +1,21 @@
 package com.skm.skmserver.service.serviceImpl;
 
+import com.skm.skmserver.dto.Item.ItemDTO;
+import com.skm.skmserver.dto.Item.UpdateItemDTO;
 import com.skm.skmserver.dto.ItemCategory.ItemCategoryDTO;
+import com.skm.skmserver.dto.ItemCategory.UpdateItemCategoryDTO;
+import com.skm.skmserver.entity.Item;
 import com.skm.skmserver.entity.ItemCategory;
 import com.skm.skmserver.repo.ItemCategoryRepository;
 import com.skm.skmserver.service.ItemCategoryService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
 public class ItemCategoryServiceImpl implements ItemCategoryService {
 
     private final ItemCategoryRepository itemCategoryRepository;
@@ -19,8 +27,32 @@ public class ItemCategoryServiceImpl implements ItemCategoryService {
         this.modelMapper=modelMapper;
     }
 
+    public List<ItemCategoryDTO> allItemCategories() {
+        return itemCategoryRepository.findAll()
+                .stream()
+                .map(itemCategory -> modelMapper.map(itemCategory, ItemCategoryDTO.class)).toList();
+    }
+
     public ItemCategoryDTO saveItemCategory(ItemCategoryDTO itemCategoryDTO){
         itemCategoryRepository.save(modelMapper.map(itemCategoryDTO, ItemCategory.class));
         return itemCategoryDTO;
+    }
+
+    public ItemCategoryDTO getItemCategory (int id) {
+        return modelMapper.map(itemCategoryRepository.findById(id), ItemCategoryDTO.class);
+    }
+
+    public ItemCategoryDTO updateItemCategory(UpdateItemCategoryDTO itemCategoryDTO, int id) {
+        ItemCategory category = itemCategoryRepository.findById(id);
+        category.setCategory_name(itemCategoryDTO.getCategory_name());
+        return modelMapper.map(itemCategoryRepository.save(category), ItemCategoryDTO.class);
+    }
+
+    public boolean deleteItemCategory(int id) {
+        if (itemCategoryRepository.findById(id) == null) {
+            return false;
+        }
+        itemCategoryRepository.deleteById(id);
+        return true;
     }
 }
