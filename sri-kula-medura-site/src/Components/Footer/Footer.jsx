01132ecdfe2a2311c970140c9df4bd70887609bf
SachiDeepashika <114232848+SachiDeepashika@users.noreply.github.com>
2023-07-08 14:19:14 +0530

    export keyword removed

--- sri-kula-medura-site/src/Components/Footer/Footer.jsx
+++ sri-kula-medura-site/src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 
-export const Footer = () => {
+const Footer = () => {
     return (
         <div className="box">
             <div className="section-wrapper">
