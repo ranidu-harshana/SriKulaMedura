ccbd2324128cd8d51963285773920d3d7057956b
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-09 01:48:54 +0530

    all reservations and one reservation

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ReservationServiceImpl.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/ReservationServiceImpl.java
@@ -1,8 +1,11 @@
 package com.skm.skmserver.service.serviceImpl;
 
+import com.skm.skmserver.dto.Item.ItemDTO;
 import com.skm.skmserver.dto.Reservation.ReservationDTO;
 import com.skm.skmserver.dto.Reservation.UpdateReservationDTO;
+import com.skm.skmserver.repo.ReservationRepository;
 import com.skm.skmserver.service.ReservationService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +14,19 @@ import java.util.List;
 @Service
 @Transactional
 public class ReservationServiceImpl implements ReservationService {
+    private final ReservationRepository reservationRepository;
+    private final ModelMapper modelMapper;
+
+    public ReservationServiceImpl(ReservationRepository reservationRepository, ModelMapper modelMapper) {
+        this.reservationRepository = reservationRepository;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public List<ReservationDTO> allReservations() {
-        return null;
+        return reservationRepository.findAll()
+                .stream()
+                .map(reservation -> modelMapper.map(reservation, ReservationDTO.class)).toList();
     }
 
     @Override
@@ -23,7 +36,7 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public ReservationDTO getReservation(int id) {
-        return null;
+        return modelMapper.map(reservationRepository.findById(id), ReservationDTO.class);
     }
 
     @Override
