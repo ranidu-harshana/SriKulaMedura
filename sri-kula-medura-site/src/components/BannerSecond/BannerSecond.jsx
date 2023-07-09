962c699c9e1848790c527fdec306d93d37ff8e56
Sachindu Malshan <sachindu.info@gmail.com>
2023-07-09 20:26:13 +0530

    Second Banner Created

---
+++ sri-kula-medura-site/src/components/BannerSecond/BannerSecond.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './BannerSecond.css';
+import arImg from '../../assets/BannerSecond/augmentedReality.png';
+import Button from 'react-bootstrap/Button';
+
+const BannerSecond = (props) => {
+    return(
+        <>
+        <containor fluid>
+            <div className="BodySecondBox">
+                <div className="row mt-3">
+                    <div className="col-md-2" ></div>
+                    <div className="col-md-4 align-self-center cntSection1">
+                        <p className="text-center fs-3 font-weight-bold arText">
+                            Experience the <br/> Augmented Reality Here
+                            <br/>
+                            <Button className="button-decoration-2 btn-md mt-4">Try Out</Button>{' '}
+                        </p>
+                    </div>
+                    <div className="col-md-4 align-self-center">
+                        <img src={arImg} className="arImg1"/>
+                    </div>
+                    <div className="col-md-2"></div>
+                </div>
+            </div>
+        </containor>
+        </>
+    );
+}
+
+export default BannerSecond;
