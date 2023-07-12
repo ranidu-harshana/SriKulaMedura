89fd4a17e2c10b8583413196f9c62072331de7d9
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-13 01:21:18 +0530

    rename

--- sri-kula-medura-site/src/layouts/MainLayout.jsx
+++ sri-kula-medura-site/src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
-import TopNav from "../components/TopNav/TopNav";
 import {Outlet} from "react-router-dom";
 import './Layout.css'
 import Footer from "../components/Footer/Footer";
+import TopNav from "../components/TopNav/TopNav";
 
 const MainLayout = (props) => {
 	return (<>
