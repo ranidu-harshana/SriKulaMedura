d5b313817f8f262a829498cc316e4343058daf27
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-14 11:22:03 +0530

    additional payment controller
    get all additional payment and store payments

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/AdditionalPaymentController.java
@@ -0,0 +1,28 @@
+package com.skm.skmserver.controller;
+
+import com.skm.skmserver.dto.AdditionalPaymentDTO;
+import com.skm.skmserver.service.serviceImpl.AdditionalPaymentServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/additional-payment")
+@RequiredArgsConstructor
+public class AdditionalPaymentController {
+    private final AdditionalPaymentServiceImpl additionalPaymentService;
+
+    @GetMapping("/")
+    public List<AdditionalPaymentDTO> index(){
+        return additionalPaymentService.allAdditionalPayments();
+    }
+
+    @PostMapping ("/")
+    public AdditionalPaymentDTO store(@RequestBody AdditionalPaymentDTO additionalPaymentDTO) {
+        return additionalPaymentService.saveAdditionalPayment(additionalPaymentDTO);
+    }
+
+}
