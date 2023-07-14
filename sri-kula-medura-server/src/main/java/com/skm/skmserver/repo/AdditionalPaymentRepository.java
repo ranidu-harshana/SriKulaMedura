628154bd75e3c618baaf238066e93f7633fc2537
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-14 13:21:37 +0530

    bug fixed

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/repo/AdditionalPaymentRepository.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/repo/AdditionalPaymentRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface AdditionalPaymentRepository extends JpaRepository<AdditionalPayment,Integer> {
-    AdditionalPaymentRepository findById(int id);
+    AdditionalPayment findById(int id);
 
 }
