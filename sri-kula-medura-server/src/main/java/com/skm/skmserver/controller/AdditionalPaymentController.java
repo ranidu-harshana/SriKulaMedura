4212260aeb23303aa076799e5390f2183bbbf321
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-14 14:03:42 +0530

    removed unwanted imports

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/AdditionalPaymentController.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/AdditionalPaymentController.java
@@ -3,8 +3,6 @@ package com.skm.skmserver.controller;
 import com.skm.skmserver.dto.AdditionalPaymentDTO;
 import com.skm.skmserver.service.serviceImpl.AdditionalPaymentServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- sri-kula-medura-server/src/main/java/com/skm/skmserver/repo/AdditionalPaymentRepository.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/repo/AdditionalPaymentRepository.java
@@ -1,11 +1,9 @@
 package com.skm.skmserver.repo;
 
 import com.skm.skmserver.entity.AdditionalPayment;
-import com.skm.skmserver.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface AdditionalPaymentRepository extends JpaRepository<AdditionalPayment,Integer> {
