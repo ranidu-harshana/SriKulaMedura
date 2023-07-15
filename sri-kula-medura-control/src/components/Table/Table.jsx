ca740ada8c3a8866ef294552824ba1e0741877c4
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-15 19:55:24 +0530

    Update Table.jsx

--- sri-kula-medura-control/src/components/Table/Table.jsx
+++ sri-kula-medura-control/src/components/Table/Table.jsx
@@ -1,16 +1,15 @@
 import './Table.css'
-import React, {useState, useEffect, useCallback, useRef} from 'react';
-import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+import React, {useCallback, useRef} from 'react';
+import { AgGridReact } from 'ag-grid-react';
 
-import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
-import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 import SearchIcon from '@mui/icons-material/Search';
 
-const Table = ({data, columnNames}) => {
+const Table = ({payload, columns}) => {
+	const windowHeight = window.innerHeight - 300
 	const gridRef = useRef();
-	const [rowData, setRowData] = useState();
-	const [columns, setColumns] = useState([]);
 
 	const defaultColDef = {
 		sortable: true
@@ -22,16 +21,6 @@ const Table = ({data, columnNames}) => {
 		);
 	}, []);
 
-	// Example load data from server
-	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/todos')
-			.then(result => result.json())
-			.then(rowData => {
-				setColumns(Object.keys(rowData[0]))
-				setRowData(rowData)
-			})
-	}, []);
-
 	return (
 		<div>
 				<div className="example-wrapper">
@@ -39,20 +28,19 @@ const Table = ({data, columnNames}) => {
 						<div className={'filter-text-container mb-2'}>
 							<div className="input-group flex-nowrap">
 								<span className="input-group-text" id="addon-wrapping"><SearchIcon/></span>
-								<input type="text" className="form-control" placeholder="Username" aria-label="Username" id="filter-text-box" onInput={onFilterTextBoxChanged}
+								<input type="text" className="form-control" placeholder="Search" aria-label="Search" id="filter-text-box" onInput={onFilterTextBoxChanged}
 								       aria-describedby="addon-wrapping"/>
 							</div>
 						</div>
 
-						<div className="ag-theme-alpine" style={{width: "100%", height: 600}}>
+						<div className="ag-theme-alpine" style={{width: "100%", height: `${windowHeight}px`}}>
 							<AgGridReact
 								ref={gridRef}
-								rowData={rowData}
-								columnDefs={columns.map(col => {
-									return {field: col, headerName: "Ranidu"}
-								})}
+								rowData={payload}
+								columnDefs={columns}
 								defaultColDef={defaultColDef}
 								pagination={true}
+								paginationAutoPageSize={true}
 							/>
 						</div>
 					</div>
