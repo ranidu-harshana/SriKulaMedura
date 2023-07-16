5d3fa2f5dbbd05af6615ff2cb684e09d37571b24
Sachindu Malshan <sachindu.info@gmail.com>
2023-07-16 17:10:57 +0530

    CardBanner Files were created

---
+++ sri-kula-medura-site/src/components/CardBanner/CardBanner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CardBanner = (props) => {
+    return(
+        <>
+            <containor fluid>
+                <div className="CardBannerBox" >
+                    <div className="row">
+                        <p className="text-center">
+                            Our Features and Services
+                        </p>
+                    </div>
+                    <div className="row">
+
+                    </div>
+                </div>
+            </containor>
+        </>
+    );
+}
+
+export default CardBanner;
