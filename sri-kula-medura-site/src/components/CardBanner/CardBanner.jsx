11608268ba6e234e72e128bb091f87c25f60db7a
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-16 21:50:11 +0530

    card is card banner as a compnent

--- sri-kula-medura-site/src/components/CardBanner/CardBanner.jsx
+++ sri-kula-medura-site/src/components/CardBanner/CardBanner.jsx
@@ -3,6 +3,7 @@ import savetime from '../../assets/CardBanner/savetime.png';
 import payment from '../../assets/CardBanner/payment.png';
 import userfriendly from '../../assets/CardBanner/userfriendly.png';
 import './CardBanner.css';
+import Card from "./Card";
 
 const CardBanner = (props) => {
     return(
@@ -17,45 +18,9 @@ const CardBanner = (props) => {
                         </div>
                     </div>
                     <div className="row justify-content-center card-all">
-                        <div className="col-md-2">
-                            <div className="card">
-                                <div className="text-center mt-4 mb-2">
-                                    <img src={savetime} className="card-img-top " alt=""/>
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center mb-3">Save Time</h5>
-                                    <p className="card-text text-justify">
-                                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloribus nam nisi quam voluptas! Deleniti ducimus impedit ipsa voluptatibus? Aspernatur
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2 mx-5">
-                            <div className="card">
-                                <div className="text-center mt-4 mb-2">
-                                    <img src={payment} className="card-img-top " alt=""/>
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center mb-3">Easy Payment</h5>
-                                    <p className="card-text text-justify">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloribus nam nisi quam voluptas! Deleniti ducimus impedit ipsa voluptatibus? Aspernatur
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div className="card">
-                                <div className="text-center mt-4 mb-2">
-                                    <img src={userfriendly} className="card-img-top " alt=""/>
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-center mb-3">User Friendly</h5>
-                                    <p className="card-text text-justify">
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloribus nam nisi quam voluptas! Deleniti ducimus impedit ipsa voluptatibus? Aspernatur
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                        <Card title={'Save Time'} image={savetime} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloribus nam nisi quam voluptas! Deleniti ducimus impedit ipsa voluptatibus? Aspernatur"}/>
+                        <Card title={'Easy Payment'} image={payment} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloribus nam nisi quam voluptas! Deleniti ducimus impedit ipsa voluptatibus? Aspernatur"}/>
+                        <Card title={'User Friendly'} image={userfriendly} content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloribus nam nisi quam voluptas! Deleniti ducimus impedit ipsa voluptatibus? Aspernatur"}/>
                     </div>
                 </div>
             </containor>
--- sri-kula-medura-site/src/pages/Home/Home.jsx
+++ sri-kula-medura-site/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import ARAdBanner from "../../components/ARAdBanner/ARAdBanner";
+import CardBanner from "../../components/CardBanner/CardBanner";
 
 const Home = (props) => {
 	return (
 		<>
 			<Header/>
 			<ARAdBanner/>
+			<CardBanner/>
 			<Footer/>
 		</>
 	);
