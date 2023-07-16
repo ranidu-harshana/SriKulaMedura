c1453412ad4cd84758f83d83afc4c301ecc19fad
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-16 17:57:58 +0530

    model mapper removed

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/SriKulaMeduraServerApplication.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/SriKulaMeduraServerApplication.java
@@ -11,9 +11,5 @@ public class SriKulaMeduraServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SriKulaMeduraServerApplication.class, args);
 	}
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
 
 }
