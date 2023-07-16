dc48e8b5e5c20431bb8d6458d27c8f0cdad3d4ac
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-16 23:43:12 +0530

    datatype fixed

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/CostDTO.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/CostDTO.java
@@ -5,16 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Date;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
 public class CostDTO {
     private int id;
-    private String transport;
+    private double transport;
     private double salary;
-    private String cleaning;
-    private String depreciation;
+    private double cleaning;
+    private double depreciation;
     private Date created_at;
     private Date updated_at;
     private int reservation_id;
