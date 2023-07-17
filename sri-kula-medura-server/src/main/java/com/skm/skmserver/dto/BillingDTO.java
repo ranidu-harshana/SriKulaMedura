7c4482f59228d49617c189f3043d381227d31967
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-17 12:55:14 +0530

    billing DTO updated

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/BillingDTO.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/BillingDTO.java
@@ -1,13 +1,23 @@
 package com.skm.skmserver.dto;
 
+import com.skm.skmserver.entity.Reservation;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
 public class BillingDTO {
+    private int id;
+    private double total_bill;
+    private double discount;
+    private double advance;
+    private Date created_at;
+    private Date updated_at;
+    private Reservation reservation;
 }
