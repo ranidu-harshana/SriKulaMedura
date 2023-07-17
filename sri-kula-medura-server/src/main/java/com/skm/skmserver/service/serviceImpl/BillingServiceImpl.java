37e99e82a36231e9a240d024ba4e167b22384ea5
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-17 16:30:53 +0530

    set method implemented in billingserviceimpl

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/BillingServiceImpl.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/BillingServiceImpl.java
@@ -1,6 +1,12 @@
 package com.skm.skmserver.service.serviceImpl;
 
+import com.skm.skmserver.dto.BillingDTO;
+import com.skm.skmserver.entity.Billing;
+import com.skm.skmserver.repo.BillingRepository;
+import com.skm.skmserver.repo.ReservationRepository;
 import com.skm.skmserver.service.BillingService;
+import com.skm.skmserver.service.MainService;
+import com.skm.skmserver.util.MapAll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +14,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class BillingServiceImpl implements BillingService {
-
+public class BillingServiceImpl implements BillingService, MainService<BillingDTO, Billing> {
+    private final BillingRepository billingRepository;
+    private final ReservationRepository reservationRepository;
+    private final MapAll<Billing, BillingDTO> mapAll;
+    
+    public BillingDTO set(Billing billing) {
+        return BillingDTO.builder()
+                .id(billing.getId())
+                .total_bill(billing.getTotal_bill())
+                .discount(billing.getAdvance())
+                .created_at(billing.getCreated_at())
+                .updated_at(billing.getUpdated_at())
+                .reservation_id(billing.getReservation().getId())
+                .build();
+    }
 }
