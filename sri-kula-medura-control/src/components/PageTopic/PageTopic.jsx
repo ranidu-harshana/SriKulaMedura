2e4b54607e45f7adbab0f12c464c952537d6f5cf
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-20 20:45:29 +0530

    Update PageTopic.jsx

--- sri-kula-medura-control/src/components/PageTopic/PageTopic.jsx
+++ sri-kula-medura-control/src/components/PageTopic/PageTopic.jsx
@@ -1,12 +1,14 @@
 import './PageTopic.css'
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import {Link} from "react-router-dom";
 
 const PageTopic = ({topic, breadcrumbs}) => {
 	return (<div className={'topic'}>
 			{topic}
 			<Breadcrumb className={'breadcrumb'}>
 				{breadcrumbs.map((breadcrumb, index) => !breadcrumb.active ?
-					<Breadcrumb.Item href={`${breadcrumb.link}`} key={index}>{breadcrumb.title}</Breadcrumb.Item> :
+					<Breadcrumb.Item key={index}>
+						<Link to={`${breadcrumb.link}`}>{breadcrumb.title}</Link></Breadcrumb.Item> :
 					<Breadcrumb.Item active key={index}>{breadcrumb.title}</Breadcrumb.Item>)}
 			</Breadcrumb>
 		</div>);
