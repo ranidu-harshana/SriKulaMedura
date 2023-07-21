458bb6b3242a0243a8e1d35bc78a59206a57a004
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-22 00:05:55 +0530

    make mobile responsive

--- sri-kula-medura-control/src/pages/Home/Home.jsx
+++ sri-kula-medura-control/src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import CalendarComp from "../../components/Calendar/CalendarComp";
+import PageTopic from "../../components/PageTopic/PageTopic";
 
 const Home = (props) => {
 	return (
 		<div>
+			<PageTopic topic={"Dashboard"} breadcrumbs={[
+				{title: 'Home', active: true},
+			]}/>
 			<CalendarComp dates={["2023-06-28", "2023-07-16", "2023-08-02", "2023-08-01"]}/>
 		</div>
 	);
