8058ce3ed2c4ca17985a78d98cf4644edb4495cd
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-22 00:09:20 +0530

    code format

--- sri-kula-medura-control/src/components/Calendar/ReservationIndicator.jsx
+++ sri-kula-medura-control/src/components/Calendar/ReservationIndicator.jsx
@@ -5,17 +5,12 @@ import IconButton from "@mui/material/IconButton";
 const ReservationIndicator = () => {
 	const matches = useMediaQuery('(max-width: 768px)')
 
-	return (
-		<>
+	return (<>
 			{matches ? <IconButton><EventIcon style={{
-				fontSize: "10px",
-				color: "green",
+				fontSize: "10px", color: "green",
 			}}/></IconButton> : <Chip color="success" size="small" label={"There are Reservations"} icon={<EventIcon/>}
 			                          className={'mt-4 px-2 py-3'}/>}
-
-
-		</>
-	)
+		</>)
 }
 
 export default ReservationIndicator
