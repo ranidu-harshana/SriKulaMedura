7fc3f77fd59d374abd463caf6022c8f094c8795f
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-24 02:14:48 +0530

    noteDTO implemeted

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/NoteDTO.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/NoteDTO.java
@@ -1,9 +1,22 @@
 package com.skm.skmserver.dto;
 
+import com.skm.skmserver.entity.Reservation;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @NoArgsConstructor
 @Data
+@AllArgsConstructor
+@Builder
 public class NoteDTO {
+    private int id;
+    private String note;
+    private boolean status;
+    private Date created_at;
+    private Date updated_at;
+    private Reservation reservation;
 }
