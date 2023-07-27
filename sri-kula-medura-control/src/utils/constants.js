b2282df2fc963fc4f71bdd9591ea019fabdfd639
Ranidu Harshana <harshanaranidum@gmail.com>
2023-07-28 02:07:22 +0530

    connect with azure

--- sri-kula-medura-control/src/utils/constants.js
+++ sri-kula-medura-control/src/utils/constants.js
@@ -1,5 +1,6 @@
 const constants = {
 	BASE_URL: "http://localhost:8080/api/v1"
+	// BASE_URL: "https://skmapi.agreeablebay-9c7abcb8.eastus.azurecontainerapps.io/api/v1"
 }
 
 export const {BASE_URL} =  constants
