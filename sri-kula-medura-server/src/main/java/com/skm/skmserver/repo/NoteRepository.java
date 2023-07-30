6bb67569e138c7110a10435937ef76b0ed31482c
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-07-30 20:41:49 +0530

    find by id added in note repository

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/repo/NoteRepository.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/repo/NoteRepository.java
@@ -1,9 +1,11 @@
 package com.skm.skmserver.repo;
 
+import com.skm.skmserver.entity.Cost;
 import com.skm.skmserver.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
+    Note findById(int id);
 }
