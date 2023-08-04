8f8f4af7e504159409a557e9b1944d61668290a8
Ranidu Harshana <harshanaranidum@gmail.com>
2023-08-04 15:29:26 +0530

    item category

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/ItemCategoryDTO.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/ItemCategoryDTO.java
@@ -1,5 +1,6 @@
 package com.skm.skmserver.dto;
 
+import com.skm.skmserver.entity.ItemCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,11 +11,20 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Builder
+@Builder(builderMethodName = "internalBuilder")
 public class ItemCategoryDTO {
     private int id;
     private String category_name;
     private Date created_at;
     private Date updated_at;
 //    private List<ItemDTO> items;
+
+    public static ItemCategoryDTOBuilder builder(ItemCategory itemCategory) {
+        return internalBuilder()
+                .id(itemCategory.getId())
+                .category_name(itemCategory.getCategory_name())
+                .created_at(itemCategory.getCreated_at())
+                .updated_at(itemCategory.getUpdated_at());
+//                .items(itemCategory.getItems().stream().map(itemService::set).toList())
+    }
 }
--- sri-kula-medura-server/src/main/java/com/skm/skmserver/entity/ItemCategory.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/entity/ItemCategory.java
@@ -1,5 +1,6 @@
 package com.skm.skmserver.entity;
 
+import com.skm.skmserver.dto.ItemCategoryDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Builder
+@Builder(builderMethodName = "internalBuilder")
 public class ItemCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,4 +37,13 @@ public class ItemCategory {
 
     @OneToMany(mappedBy = "item_category")
     private List<Item> items;
+
+    public static ItemCategoryBuilder builder(ItemCategory itemCategory) {
+        return internalBuilder()
+                .id(itemCategory.getId())
+                .category_name(itemCategory.getCategory_name())
+                .created_at(itemCategory.getCreated_at())
+                .updated_at(itemCategory.getUpdated_at());
+//                .items(itemCategory.getItems().stream().map(itemService::set).toList())
+    }
 }
