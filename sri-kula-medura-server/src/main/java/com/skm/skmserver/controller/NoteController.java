c703d86ad19ff1a43fa6599e432326dcc833fd23
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-08-05 23:14:32 +0530

    note controller get all notes

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/NoteController.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/NoteController.java
@@ -1,11 +1,26 @@
 package com.skm.skmserver.controller;
 
+import com.skm.skmserver.dto.NoteDTO;
+import com.skm.skmserver.service.NoteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "api/v1/notes")
+@RequestMapping(value = "api/v1/note")
 @CrossOrigin
+@RequiredArgsConstructor
 public class NoteController {
+    private final NoteService noteService;
+
+    @GetMapping("/")
+    public List<NoteDTO> index(){
+        return noteService.allNotes();
+    }
+
+
 }
