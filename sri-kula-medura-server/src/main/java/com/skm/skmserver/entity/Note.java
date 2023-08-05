2baa896bcf92eeb03af58285dde379f78e8bafa4
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-08-05 23:23:56 +0530

    builder added to note entity

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/entity/Note.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/entity/Note.java
@@ -2,6 +2,7 @@ package com.skm.skmserver.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
