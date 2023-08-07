23c68b88d45173fcf4924ec0c9855d24f4402940
Ranidu Harshana <harshanaranidum@gmail.com>
2023-08-07 17:55:29 +0530

    Update measurementSlice.js

--- sri-kula-medura-control/src/store/slices/measurementSlice.js
+++ sri-kula-medura-control/src/store/slices/measurementSlice.js
@@ -2,20 +2,7 @@ import {createEntityAdapter, createSlice} from "@reduxjs/toolkit";
 
 const measurementState = createEntityAdapter({selectId: measurement => measurement.id})
 
-const initialState = measurementState.getInitialState({
-	status: "",
-	single_measurement: {
-		id: null,
-		created_at: null,
-		item_code: null,
-		item_image_url: null,
-		item_name: null,
-		item_type: null,
-		rented_status: null,
-		updated_at: null,
-		item_category_id: null
-	}
-})
+const initialState = measurementState.getInitialState()
 
 const measurementSlice = createSlice({
 	name: 'measurement',
