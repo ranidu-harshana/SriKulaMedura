1f843fd0ef4117cb44a27b0ddbca6a4bc307bf13
Sachindu Malshan <sachindu.info@gmail.com>
2023-08-07 16:01:33 +0530

    Navbar shadow error and tab space fixed

--- sri-kula-medura-site/src/components/TopNav/TopNav.jsx
+++ sri-kula-medura-site/src/components/TopNav/TopNav.jsx
@@ -8,29 +8,35 @@ import logo from '../../assets/logos/srikulaLogo.png';
 const TopNav = (props) => {
 	return (
 		<div className="gradient-custom">
-      <Navbar expand="lg" className="pt-2 pb-2" >
-        <Container fluid>
-          <Navbar.Brand href="#">
-          <img src={logo} alt="Logo" width={70} height={'auto'} className="logo-alignment"/>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Nav style={{fontWeight:"Bold", marginTop:"10px", color:"white"}}>
-              <Nav.Link href="#" className="text-white">Home</Nav.Link>
-              <Nav.Link href="#" className="text-white">About Us</Nav.Link>
-              <Nav.Link href="#" className="text-white">FAQ</Nav.Link>
-              <Nav.Link href="#">
-                <Button variant="light" size="md" className="rounded-pill button-decoration">Try Outfit</Button>{' '}
-                </Nav.Link>              
-            </Nav>
-            <span style={{color:"white"}}>
-              <PersonIcon className="mx-2" /> |
-              <FacebookIcon className="mx-2" />
-              <InstagramIcon/>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+          <Navbar expand="lg" className="pt-2 pb-2 shadow-none" >
+            <Container fluid>
+              <Navbar.Brand href="#">
+              <img src={logo} alt="Logo" width={70} height={'auto'} className="logo-alignment"/>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                <Nav style={{fontWeight:"Bold", marginTop:"10px", color:"white"}}>
+                  <Nav.Item className="mx-2">
+                    <Nav.Link href="#" className="text-white" >Home</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item className="mx-2">
+                    <Nav.Link href="#" className="text-white">About Us</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item className="mx-2">
+                    <Nav.Link href="#" className="text-white">FAQ</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item className="mx-2">
+                    <Nav.Link href="#">
+                      <Button variant="light" size="md" className="rounded-pill button-decoration">Try Outfit</Button>{' '}
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <span style={{color:"white"}}>
+                  <PersonIcon className="mx-2" />
+                </span>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
     </div>
 	);
 }
