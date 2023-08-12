e3c9bddeea650794f38ff03744117969efee7b7f
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-08-13 03:14:34 +0530

    bug fixed

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/InterimPaymentServiceImpl.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/serviceImpl/InterimPaymentServiceImpl.java
@@ -5,6 +5,7 @@ import com.skm.skmserver.entity.InterimPayment;
 import com.skm.skmserver.repo.InterimPaymentRepository;
 import com.skm.skmserver.repo.ReservationRepository;
 import com.skm.skmserver.service.InterimPaymentService;
+import com.skm.skmserver.service.MainService;
 import com.skm.skmserver.util.MapAll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,16 @@ import java.util.List;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class InterimPaymentServiceImpl implements InterimPaymentService {
+public class InterimPaymentServiceImpl implements InterimPaymentService , MainService<InterimPaymentDTO,InterimPayment> {
     private final InterimPaymentRepository interimPaymentRepository;
     private final ReservationRepository reservationRepository;
     private final MapAll<InterimPayment , InterimPaymentDTO> mapAll;
 
     public List<InterimPaymentDTO> allInterimPayments(){
         return mapAll.mapAllAttributesToDTO(interimPaymentRepository.findAll(),this);
+    }
+
+    public InterimPaymentDTO set(InterimPayment interimPayment) {
+        return InterimPaymentDTO.builder(interimPayment).build();
     }
 }
