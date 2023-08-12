8302afcd2c8d16279d4584b2aa4c760ff8291a16
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-08-13 02:32:14 +0530

    IP repository implemented

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/repo/InterimPaymentRepository.java
@@ -0,0 +1,10 @@
+package com.skm.skmserver.repo;
+
+import com.skm.skmserver.entity.InterimPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InterimPaymentRepository extends JpaRepository<InterimPayment ,Integer> {
+    InterimPayment findAllBy(int id);
+}
