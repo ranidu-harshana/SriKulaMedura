4abd57a8754a3de45dbf30bed3cf428ccfdf8206
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-08-13 02:41:00 +0530

    internalbuilder implemeted

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/InterimPaymentDTO.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/dto/InterimPaymentDTO.java
@@ -1,7 +1,6 @@
 package com.skm.skmserver.dto;
 
-import com.skm.skmserver.entity.Customer;
-import com.skm.skmserver.entity.Reservation;
+import com.skm.skmserver.entity.InterimPayment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Builder
+@Builder(builderMethodName = "internalBuilder")
 public class InterimPaymentDTO {
     private int id;
     private double interim_payment;
@@ -20,4 +19,14 @@ public class InterimPaymentDTO {
     private Date updated_at;
     private int reservation_id;
     private int customer_id;
+
+    public static InterimPaymentDTOBuilder builder(InterimPayment interimPayment){
+        return internalBuilder()
+                .id(interimPayment.getId())
+                .interim_payment(interimPayment.getInterim_payment())
+                .created_at(interimPayment.getCreated_at())
+                .updated_at(interimPayment.getUpdated_at())
+                .reservation_id(interimPayment.getReservation().getId())
+                .customer_id(interimPayment.getCustomer().getId());
+    }
 }
