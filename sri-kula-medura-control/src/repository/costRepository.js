27ca85603663e6a34daec1f84183fd899c534de3
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-08-27 18:46:01 +0530

    fixed

--- sri-kula-medura-control/src/repository/costRepository.js
+++ sri-kula-medura-control/src/repository/costRepository.js
@@ -1,7 +1,6 @@
 import Repository from "../utils/Repository";
 
 const storeCost = (reservation_id, cost, reason ) => {
-    console.log(reservation_id, cost, reason)
     return Repository.post('/cost/', {reservation_id, other_cost:cost, reason})
 }
 
