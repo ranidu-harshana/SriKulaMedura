501714ccf85df052264c015989de122e71b0040c
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-09-05 22:59:06 +0530

    service fixed

---
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/OtherCostService.java
@@ -0,0 +1,7 @@
+package com.skm.skmserver.service;
+
+import com.skm.skmserver.dto.OtherCostDTO;
+
+public interface OtherCostService {
+    OtherCostDTO saveOtherCost(OtherCostDTO otherCostDTO);
+}
