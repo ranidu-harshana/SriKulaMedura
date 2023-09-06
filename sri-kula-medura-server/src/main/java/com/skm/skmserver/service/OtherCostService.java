c4546bd769d12d0200ae294bd69dc01ce8d0cdad
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-09-06 15:37:49 +0530

    updated service

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/service/OtherCostService.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/service/OtherCostService.java
@@ -2,7 +2,11 @@ package com.skm.skmserver.service;
 
 import com.skm.skmserver.dto.OtherCostDTO;
 
+import java.util.List;
+
 public interface OtherCostService {
     OtherCostDTO saveOtherCost(OtherCostDTO otherCostDTO);
     OtherCostDTO updateOtherCost(OtherCostDTO otherCostDTO, int id);
+    List<OtherCostDTO> allOtherCosts();
+    boolean deleteOtherCost(int id);
 }
