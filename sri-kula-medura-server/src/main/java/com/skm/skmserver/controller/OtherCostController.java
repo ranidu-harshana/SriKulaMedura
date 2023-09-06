4d3cc0e3e1580d95b63c650bc3844ea354eedb80
DanilkaShalinda <73026664+DanilkaShalinda@users.noreply.github.com>
2023-09-06 15:32:00 +0530

    update and delete other cost controller fixed

--- sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/OtherCostController.java
+++ sri-kula-medura-server/src/main/java/com/skm/skmserver/controller/OtherCostController.java
@@ -1,6 +1,5 @@
 package com.skm.skmserver.controller;
 
-import com.skm.skmserver.dto.AdditionalPaymentDTO;
 import com.skm.skmserver.dto.OtherCostDTO;
 import com.skm.skmserver.service.serviceImpl.OtherCostServiceImpl;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +22,16 @@ public class OtherCostController {
     @PostMapping("/")
     public OtherCostDTO store(@RequestBody OtherCostDTO otherCostDTO){
         return otherCostService.saveOtherCost(otherCostDTO);
+    }
+
+    @PutMapping("/{id}")
+    public OtherCostDTO update(@RequestBody OtherCostDTO otherCostDTO, @PathVariable int id){
+        return otherCostService.updateOtherCost(otherCostDTO,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id){
+        return otherCostService.deleteOtherCost(id) ? "Other Cost Deleted Successfully" : "Other cost Delete Failed";
     }
 
 }
