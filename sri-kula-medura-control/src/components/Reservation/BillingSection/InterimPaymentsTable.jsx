713cc2507186f3320e7f5442b29c6443d6e45590
Danilka Dissanayaka <ddissanayaka@lk.onsystechnologies.com>
2023-12-17 20:03:49 +0530

    delete interim payment

--- sri-kula-medura-control/src/components/Reservation/BillingSection/InterimPaymentsTable.jsx
+++ sri-kula-medura-control/src/components/Reservation/BillingSection/InterimPaymentsTable.jsx
@@ -1,14 +1,17 @@
 import {ButtonGroup} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
-import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import {useParams} from "react-router-dom";
 import useBillGenerator from "../../../hooks/useBillGenerator";
 import {useEffect} from "react";
-import {addInterimPayments, clearInterimPaymentState} from "../../../store/slices/InterimPaymentSlice";
-import {getAllInterimPaymentsOfReservation} from "../../../repository/InterimPaymentRepository";
+import {addInterimPayments, clearInterimPaymentState,deleteInterimPayments,updateInterimPayments} from "../../../store/slices/InterimPaymentSlice";
+import {getAllInterimPaymentsOfReservation,deleteInterimPayment,updateInterimPayment} from "../../../repository/InterimPaymentRepository";
 import {useDispatch} from "react-redux";
-
+import Swal from "sweetalert2";
+import {formatTime} from "../../../utils/formatTime";
+import EditIcon from "@mui/icons-material/Edit";
+import {updateNote} from "../../../repository/noteRepository";
+import {updateNotes} from "../../../store/slices/noteSlice";
 const InterimPaymentsTable = () => {
 	const {id} = useParams()
 	const {interimPayments} = useBillGenerator(id)
@@ -25,6 +28,34 @@ const InterimPaymentsTable = () => {
 				})
 		}
 	}, [dispatcher, id, interimPayments])
+
+	const handleDeleteInterimPayment = (id) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You won't be able to revert this!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!"
+		}).then((result) => {
+			if (result.isConfirmed) {
+				deleteInterimPayment(id)
+					.then(() => {
+						Swal.fire({
+							title: "Deleted!",
+							text: "Successfully Deleted",
+							icon: "success"
+						});
+						dispatcher(deleteInterimPayments(id))
+					})
+
+					.catch(error => {
+						console.error('Error deleting note:', error);
+					});
+			}
+		});
+	};
 
 	return (
 		<>
@@ -49,14 +80,16 @@ const InterimPaymentsTable = () => {
 								<tr key={index}>
 									<th scope="row">{interimPayment.id}</th>
 									<td>{interimPayment.interim_payment}</td>
-									<td>{interimPayment.created_at}</td>
+									<td>{formatTime(interimPayment.created_at)}</td>
 									<td>
 										<ButtonGroup size="small">
-											<IconButton color="success" size="small">
-												<EditIcon />
+											<IconButton color="success" size="small"
+														onClick={() => handleUpdateInterimPayment(interimPayment.id, interimPayment.interim_payment,interimPayment.reservation_id)}>
+												<EditIcon/>
 											</IconButton>
-											<IconButton sx={{color:"red"}} size="small">
-												<DeleteIcon />
+											<IconButton sx={{color: "red"}} size="small"
+														onClick={() => handleDeleteInterimPayment(interimPayment.id)}>
+												<DeleteIcon/>
 											</IconButton>
 										</ButtonGroup>
 									</td>
--- sri-kula-medura-control/src/store/slices/InterimPaymentSlice.js
+++ sri-kula-medura-control/src/store/slices/InterimPaymentSlice.js
@@ -13,11 +13,23 @@ const interimPaymentSlice = createSlice({
                 interimPaymentState.upsertMany(state, action.payload)
             }
         },
+        updateInterimPayments: {
+            reducer: (state, action) => {
+                const {id, reservation_id, interim_payment} = action.payload;
+                interimPaymentState.upsertOne(state, {
+                    id:id,
+                    interim_payment: interim_payment,
+                    reservation_id: reservation_id
+                })
+            }
+        },
+
         saveInterimPayment: interimPaymentState.addOne,
+        deleteInterimPayments:interimPaymentState.removeOne,
         clearInterimPaymentState: interimPaymentState.removeAll
     }
 })
 
-export const {addInterimPayments, saveInterimPayment, clearInterimPaymentState} = interimPaymentSlice.actions
+export const {addInterimPayments, saveInterimPayment, clearInterimPaymentState,deleteInterimPayments,updateInterimPayments} = interimPaymentSlice.actions
 export const { selectAll: selectAllInterimPayment } = interimPaymentState.getSelectors(store => store.interim_payment);
 export default interimPaymentSlice.reducer
